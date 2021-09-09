a86917f60f211ba3ca41ddccd12ed03a0462fd18
SorRyiMDumB <ahmadyyaseen12@gmail.com>
2021-09-09 15:25:38 +1000

    made an ai

--- pain peko/steal.py
+++ pain peko/steal.py
@@ -1,26 +1,45 @@
 import cv2
+import os
+import numpy as n
 
-frameWidth = 640
-frameHeight = 480
+thres = 0.5
 
-# 0 = webcam
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+
 cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
+cap.set(3,1280)
+cap.set(4,720)
+cap.set(10,70)
+
+img = 'pain peko/lena.png'
 
 classNames = []
-classfile = "coco.names"
+classfile = 'pain peko/test.names'
 with open(classfile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
-print(classNames)
+configPath = 'pain peko/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightPath = 'pain peko/frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightPath, configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
 
 while True:
-    success, img = cap.read()
-    
-    cv2.imshow("Cutecumber Dectection Software",img)
-    
-    #closes with 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
+    success,img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold = thres)
+    print(classIds,bbox)
+
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+    cv2.imshow('Output', img)
+    cv2.waitKey(1)
