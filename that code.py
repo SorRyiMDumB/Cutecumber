fafb22a3750cb198ec2a90b1406db9da817c8a95
nyci to <87402547+nycishesh@users.noreply.github.com>
2021-11-18 14:02:09 +1100

    Add files via upload

---
+++ that code.py
@@ -0,0 +1,23 @@
+x = (input("Foodstuff: "))
+
+if x == 'banana':
+    print ('89 calories per 100 grams')
+elif x == 'apple':
+    print ('52 calories per 100 grams')
+elif x == 'sandwich':
+    print ('20 - 600 calories per 100 grams')
+elif x == 'orange':
+    print ('47 calories per 100 grams')
+elif x == 'broccoli':
+    print ('34 calories per 100 grams')
+elif x == 'carrot':
+    print ('41 calories per 100 grams')
+elif x == 'hot dog':
+    print ('290 calories per 100 grams')
+elif x == 'pizza':
+    print ('266 calories per 100 grams')
+elif x == 'donut':
+    print ('452 calories per 100 grams')
+elif x == 'cake':
+    print ('257 calories per 100 grams')
+
